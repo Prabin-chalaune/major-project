276e9964e93fee0621c0438b1396bb45a61d2008
samirbajg <sameerbajg123@gmail.com>
2025-03-05 21:29:09 +0545

    update register model

--- backend/myapp/admin.py
+++ backend/myapp/admin.py
@@ -5,7 +5,7 @@ from .models import RoomInterior
 
 # Register your models here.
 class RoomAdmin(admin.ModelAdmin):
-    list_display = ["id", "room_type", "room_image_url"]
+    list_display = ["room_type", "room_image_url"]
     list_display_links = ["room_type"]
     list_filter = ["room_type"]
     search_fields = ["room_type"]
